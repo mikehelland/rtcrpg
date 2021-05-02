3dd3c6828978403577edd31bf3c00a8fbd28e088
mikehelland <mike@mikehelland.com>
2021-05-02 15:41:10 -0700

    map embed size

--- www/map-embed.js
+++ www/map-embed.js
@@ -1,19 +1,20 @@
 function OMGEmbeddedViewerMAP (viewer) {
-    this.canvas = document.createElement("canvas")
-    this.context = this.canvas.getContext("2d")
     this.tileSize = 16
     this.img = {
         characters: document.getElementById("characters-spritesheet"),
         tiles: {}
     }
     
-    this.canvas.style.width = "100%"
-    this.canvas.style.height = "100%"
-    viewer.embedDiv.appendChild(this.canvas)
-
     import(omg.apps["rtcrpg"].path + "rpgmap.js").then(o => {
         var OMGRPGMap = o.default
         this.map = new OMGRPGMap(viewer.data, {div: viewer.embedDiv})
+        if (viewer.params && viewer.params.maxHeight) {
+            this.map.canvas.style.width = "100%"
+            this.map.canvas.style.height = "100%"
+            viewer.embedDiv.style.height = viewer.params.maxHeight + "px"
+            console.log(viewer.params.maxHeight)
+        }
+
         this.map.draw() 
     })
     
